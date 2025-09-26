bbaeed496dd71944cc294d1595192ad38824c9f2
Kamil <kamilgolub0@gmail.com>
2025-09-26 22:32:00 +0200

    Move lsp keybinds to keybinds.lua

--- src/lua/keybinds.lua
+++ src/lua/keybinds.lua
@@ -1,15 +1,51 @@
-local function map(m, k, v)
-    vim.keymap.set(m, k, v, { silent = true })
+local builtin = require('telescope.builtin')
+
+local function map(m, k, v, opts)
+    vim.keymap.set(m, k, v, opts)
 end
 
-vim.api.nvim_set_keymap('n', '<leader>hh', ':ClangdSwitchSourceHeader<CR>', { noremap = true, silent = true })
+-- Clang
+map('n', '<leader>hh', ':ClangdSwitchSourceHeader<CR>', { noremap = true, silent = true })
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<C-j>', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+--Telescope
+map('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
+map('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+map('n', '<C-j>', builtin.buffers, { desc = 'Telescope buffers' })
+map('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
+--Lazygit
+map("n", "<leader>gg", "<cmd>LazyGit<CR>", { desc = "Open LazyGit" })
 
-vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>", { desc = "Open LazyGit" })
+--LSP
+local opts = { noremap=true, silent=true }
+map('n', '<space>e', vim.diagnostic.open_float, opts)
+map('n', '[d', vim.diagnostic.goto_prev, opts)
+map('n', ']d', vim.diagnostic.goto_next, opts)
+map('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local lsp_on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  map('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  map('n', 'gd', vim.lsp.buf.definition, bufopts)
+  map('n', 'K', vim.lsp.buf.hover, bufopts)
+  map('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  map('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  map('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  map('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  map('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  map('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  map('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  map('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  map('n', 'gr', vim.lsp.buf.references, bufopts)
+  map('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+end
 
